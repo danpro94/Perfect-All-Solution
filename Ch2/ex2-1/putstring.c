13bbf2d24580d00a1d0ae4d10cec2890cae7802c
Dooy <dooy94@gmail.com>
2016-10-11 22:53:12 +0900

    Ch02 Project's 수행

---
+++ Ch2/ex2-1/putstring.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	puts("첫 C 프로그램!");
+
+	return 0;
+}
---
+++ Ch2/exMidcheck/Midcheck.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	puts("소스작성");
+	puts("빌드");
+	puts("실행");
+
+	return 0;
+}
